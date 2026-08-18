8643d26bee36723287e4093b32d37aa2f88b937c
Ex3cutor76-V1 <rusksoviski@gmail.com>
2026-08-18 15:43:41 -0300

    Primeira versão do SeeShell

---
+++ verbose.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Organizando aliases..."
+
+cat << 'EOF' >> ~/.bashrc
+# ----------------------------------
+# Aliases do Verbose Mode
+# ----------------------------------
+
+alias mkdir='mkdir -v'
+alias cp='cp -v'
+alias mv='mv -v'
+alias ln='ln -v'
+alias rm='rm -v'
+alias curl='curl -v'
+alias wget='wget -v'
+alias chmod='chmod -v'
+alias chown='chown -v'
+alias chgrp='chgrp -v'
+alias tar='tar -v'
+alias rsync='rsync -v'
+alias ssh='ssh -v'
+alias sftp='sftp -v'
+alias scp='scp -v'
+alias mount='mount -v'
+alias umount='umount -v'
+alias head='head -v'
+alias tail='tail -v'
+
+EOF
+
+echo "Completo!"
+echo "Utilize: source ~/.bashrc"
+echo "Para alterar as modificações do Shell"
